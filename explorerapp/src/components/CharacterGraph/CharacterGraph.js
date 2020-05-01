a48fc4da4c6ebea6107534940f75c0beb08d58f1
Aaron D'souza <aaron81@gmail.com>
2020-05-01 22:07:02 +0530

    Added sample graph | Aaron

--- explorerapp/src/components/CharacterGraph/CharacterGraph.js
+++ explorerapp/src/components/CharacterGraph/CharacterGraph.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Graph } from "react-d3-graph";
+import './CharacterGraph.scss';
 
-const CharacterGraph = (props) => (
-  <div>
-      Graph coming soon...
-  </div>
-);
+const CharacterGraph = (props) => {
+  const data = {
+    nodes: [{ id: "Harry" }, { id: "Sally" }, { id: "Alice" }],
+    links: [
+        { source: "Harry", target: "Sally" },
+        { source: "Harry", target: "Alice" },
+    ],
+};
+const myConfig = {
+  nodeHighlightBehavior: true,
+  node: {
+      color: "lightgreen",
+      size: 1000,
+      highlightStrokeColor: "blue",
+  },
+  link: {
+      highlightColor: "lightblue",
+  },
+  height: window.innerHeight,
+  width: window.innerWidth,
+};
+  return (
+    <div className="graph">
+      <Graph
+        id="graph-id"
+        data={data}
+        config={myConfig}
+      ></Graph>
+    </div>
+  );
+}
 
 export default CharacterGraph;
